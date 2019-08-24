62345c381efd85354b6eb23b86b03af4974e03d3
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-24 17:47:33 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,7 @@ def happy_birthday(birthday_kids)
 end
 
 
-def age_appropriate_birthday(name,age)
+def age_appropriate_birthday(birthday_kids)
   name.each do |names,age|
    puts "Happy Birthday #{names}! You are now #{age} years old"
 end
