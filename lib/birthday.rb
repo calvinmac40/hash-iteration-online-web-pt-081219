dfd102552884c29e875cf220ffce7a1c6886f40a
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-24 18:26:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,7 +16,7 @@ def age_appropriate_birthday(birthday_kids)
     if age < 12 
       puts "Happy Birthday #{names}! You are now #{age} years old!"
    else 
-     puts "You are now #{age} old! You are too old for this."
+     puts You are too old for this."
      end
      end
    end
